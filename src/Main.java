4f7d795689d2fb8e2d676fffb75c0935003a8073
Jordan Subocz <37275222+jordan5034@users.noreply.github.com>
2024-11-18 09:51:00 -0500

    first commit from laptop

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Git is working!");
+        System.out.println("test");
 
         // While working, I am going to be committing/pushing my code to the "WIP" branch
-        Main.test();
 
-    }
-
-    public static void test(){
-        System.out.println("Git is working! 2x");
         }
-
     }
 
